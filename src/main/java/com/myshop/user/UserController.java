628c78cd7460f4c31dd569db7837640538a61870
test <test@test.com>
2023-11-21 17:28:13 +0900

    #6 카카오 로그인 API 구현

---
+++ src/main/java/com/myshop/user/UserController.java
@@ -0,0 +1,40 @@
+package com.myshop.user;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	@GetMapping("/kakao/oauth")
+	public String oauthCallback(String code) {
+		RestTemplate rt = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "5fe0b7f721365d472ac1462d40d94415");
+		params.add("redirect_uri", "http://site-name/user/kakao/oauth");
+		params.add("code", code);
+
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST, // 요청할 방식
+				kakaoTokenRequest, // 요청할 때 보낼 데이터
+				String.class // 요청 시 반환되는 데이터 타입
+		);
+		return "응답 결과" + response;
+	}
+}
--- src/main/webapp/WEB-INF/jsp/user/signIn.jsp
+++ src/main/webapp/WEB-INF/jsp/user/signIn.jsp
@@ -21,7 +21,9 @@
 				</div>
 			</div>
 			<button type="button" id="loginBtn" class="btn btn-lg btn-primary w-100 my-3">로그인</button>
-			<!-- 카카오 로그인 API -->
+			<a href="https://kauth.kakao.com/oauth/authorize?client_id=5fe0b7f721365d472ac1462d40d94415&redirect_uri=http://localhost/site-name&response_type=code">
+				<img src="/static/img/kakao_login_large_wide.png" width="510">
+			</a>
 			<div class="linkArea d-flex justify-content-center">
 				<ul>
 					<li><a href="#">아이디 찾기</a></li>
