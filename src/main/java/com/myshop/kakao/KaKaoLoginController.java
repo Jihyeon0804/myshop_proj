bf56dd111c51b6cb884d29cf6f6194db864de0cc
test <test@test.com>
2023-11-25 03:35:21 +0900

    #6 카카오 로그인 API 수정

--- src/main/java/com/myshop/kakao/KaKaoLoginController.java
+++ src/main/java/com/myshop/kakao/KaKaoLoginController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.myshop.kakao.bo.KaKaoBO;
 import com.myshop.user.bo.UserBO;
+import com.myshop.user.domain.User;
 
 import lombok.AllArgsConstructor;
 
@@ -26,40 +27,53 @@ public class KaKaoLoginController {
 
 	@Autowired
 	private KaKaoBO kakaoBO;
-	
+
 	@Autowired
 	private UserBO userBO;
-	
+
 	@Transactional
 	@GetMapping("/login/result")
-	public void kakaoConnect(
-			@RequestParam("code") String code,
-			HttpSession session) {
+	public String kakaoLogin(@RequestParam("code") String code, HttpSession session) {
 		Logger logger = LoggerFactory.getLogger(this.getClass());
 		logger.info("################## code : {}", code);
-		
-        
+
 		String access_Token = kakaoBO.getAccessToken(code);
 		logger.info("################## code : {}", code);
-		System.out.println("###access_Token#### : " + access_Token);
-		
+
 		Map<String, Object> userInfo = new HashMap<>();
 		userInfo = kakaoBO.getUserInfo(access_Token);
 		logger.info("################## access_Token : {}", access_Token);
 		logger.info("################## kakao_name : {}", userInfo.get("name"));
 		logger.info("################## kakao_id : {}", userInfo.get("loginId"));
 		logger.info("################## kakao_email : {}", userInfo.get("email"));
-		String loginId = (String)userInfo.get("loginId");
-		String name = (String)userInfo.get("name");
-		String email = (String)userInfo.get("email");
+		String loginId = (String) userInfo.get("loginId");
+		String name = (String) userInfo.get("name");
+		String email = (String) userInfo.get("email");
 		String password = "kakao1122";
 		String phoneNumber = null;
 		String birth = null;
 		String joinType = "소셜";
-		
-		
-		userBO.addUser(loginId, password, name, email, phoneNumber, birth, joinType);
-		
-		
+
+		User user = userBO.existUserByLoginIdAndPassword(loginId, password);
+		if (user == null) {
+			userBO.addUser(loginId, password, name, email, phoneNumber, birth, joinType);
+			user = userBO.existUserByLoginIdAndPassword(loginId, password);
+			session.setAttribute("userLoginId", user.getLoginId());
+			session.setAttribute("userName", user.getName());
+			session.setAttribute("email", user.getEmail());
+			session.setAttribute("phoneNumber", user.getPhoneNumber());
+			session.setAttribute("birth", user.getBirth());
+			session.setAttribute("address", user.getAddress());
+			session.setAttribute("grade", user.getGrade());
+		} else {
+			session.setAttribute("userLoginId", user.getLoginId());
+			session.setAttribute("userName", user.getName());
+			session.setAttribute("email", user.getEmail());
+			session.setAttribute("phoneNumber", user.getPhoneNumber());
+			session.setAttribute("birth", user.getBirth());
+			session.setAttribute("address", user.getAddress());
+			session.setAttribute("grade", user.getGrade());
+		}
+		return "redirect:/site-name";
 	}
 }
