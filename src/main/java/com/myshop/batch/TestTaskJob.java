aa13e1530e4d58607861a8ec37f12b14f8ffb681
학원 <test@test.com>
2023-11-28 21:44:05 +0900

    배치 테스트

---
+++ src/main/java/com/myshop/batch/TestTaskJob.java
@@ -0,0 +1,17 @@
+package com.myshop.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTaskJob {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	// @Scheduled(cron = "1 * * * * *")
+	public void test() {
+		// job 내용
+		logger.info("################# test job 수행");
+	}
+}
